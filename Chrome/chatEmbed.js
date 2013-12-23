0ec9e62683697cf467da914d6e356db062e3247c
Peter Andringa <peter9221@gmail.com>
2013-12-23 01:00:10 -0500

    better for some services

--- Chrome/chatEmbed.js
+++ Chrome/chatEmbed.js
@@ -15,16 +15,26 @@ var loadEmbeds = function(){
 	links.css("color", "red");
 	links.each(function(i){
 		var link = $(this);
-		link.after('<div class="embedLoading"><i class="fa fa-spinner fa-spin fa-3x"></i></div>');
+		link.after('<div class="extension-embed-loading"><i class="fa fa-spinner fa-spin fa-3x"></i></div>');
 		requestEmbed(link.attr('href'), $('._53').width())
 			.done(function(data){
-				link.next().after(data.html);
+				link.next().after('<div class="extension-embedded-content">'+parseData(data)+'</div');
 				setTimeout(function(){
 					link.next().remove();
 				}, 1000);
 			});
 	});
 }
+
+var parseData = function(data){
+	if(data.html)
+		return data.html;
+	else if(data.thumbnail_url) //for stuff like Instagram and Xkcd that don't have embedded iframes
+		return '<img src="'+data.thumbnail_url+'"/>';
+	else
+		return "";
+}
+
 var checkLoad = function(){
 	if($("._38").exists()){
 		loadEmbeds();
